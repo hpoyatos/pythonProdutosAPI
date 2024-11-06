9e591bacb229b8d937b874f8c82f8eaaae56fcac
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-11-05 21:29:06 -0300

    Valida TOKEN
    Valida TOKEN

--- app.py
+++ app.py
@@ -3,15 +3,47 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import jwt
 import datetime
+from functools import wraps
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'seuSegredoJWT'  # Defina uma chave secreta real aqui
 
-# Vamos fazer a consulta de produtos
+# Middleware para verificar o token
+def verify_token(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-token')
+        if not token:
+            return jsonify({'error': 'Nenhum token foi fornecido.'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            request.usuario = data
+        except:
+            return jsonify({'error': 'Falha ao autenticar o token.'}), 500
+        return f(*args, **kwargs)
+    return decorated
 
+
+# Middleware para verificar token de admin
+def verify_admin_token(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-token')
+        if not token:
+            return jsonify({'error': 'Nenhum token foi fornecido.'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            if data['tipo'] != 'admin':
+                return jsonify({'error': 'Você não tem privilégios para a ação desejada.'}), 403
+            request.usuario = data
+        except:
+            return jsonify({'error': 'Falha ao autenticar o token.'}), 500
+        return f(*args, **kwargs)
+    return decorated
 
 
 # Rota para obter produtos
 @app.route('/api/produtos', methods=['GET'])
+@verify_token
 def get_produtos():
     database = sqlite3.connect("produtos.db")
     cursor = database.cursor()
@@ -29,6 +61,7 @@ def get_produtos():
 
 # Rota para obter um produto por seu id.
 @app.route('/api/produtos/<int:id>', methods=['GET'])
+@verify_token
 def get_produto(id):
     database = sqlite3.connect("produtos.db")
     cursor = database.cursor()
@@ -46,6 +79,7 @@ def get_produto(id):
 
 # Rota para criar um produto
 @app.route('/api/produtos', methods=['POST'])
+@verify_admin_token
 def create_produto():
     try:
         data = request.get_json()
@@ -67,6 +101,7 @@ def create_produto():
 
 #Rota de atualizar produto
 @app.route('/api/produtos/<int:id>', methods=['PATCH'])
+@verify_admin_token
 def atualizar_produto(id):
     try:
         data = request.get_json()
@@ -112,11 +147,13 @@ def atualizar_produto(id):
         return jsonify({"error": "Dados obrigatórios não foram fornecidos no corpo da requisição"}), 500 
 
 @app.route('/api/produtos', methods=['PATCH', 'DELETE'])
+@verify_admin_token
 def deu_ruim():
     return jsonify({"error": "O id do produto a ser atualizado ou apagado não foi fornecido como parâmetro"}), 500 
     
 # Rota para apagar um produto
 @app.route('/api/produtos/<int:id>', methods=['DELETE'])
+@verify_admin_token
 def apagar_produto(id):
     database = sqlite3.connect("produtos.db")
     cursor = database.cursor()    
@@ -130,6 +167,7 @@ def apagar_produto(id):
 # A PARTE USUARIOS
 # Rota para criar um usuario com senha criptografada
 @app.route('/api/usuarios', methods=['POST'])
+@verify_admin_token
 def create_usuario():
     try:
         data = request.get_json()
